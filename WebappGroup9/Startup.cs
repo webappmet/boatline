68fc6feba8462718493b7027a2347357b3b5f830
Erik Storås Sommer <erikssommer@gmail.com>
2021-09-22 12:30:14 +0200

    Added folder and file for logging on startup

--- WebappGroup9/Startup.cs
+++ WebappGroup9/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using WebappGroup9.DAL;
 
 namespace WebappGroup9
@@ -19,11 +20,12 @@ namespace WebappGroup9
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                loggerFactory.AddFile("Logs/log.txt");
                 DbInit.Initialize(app);
             }
 
