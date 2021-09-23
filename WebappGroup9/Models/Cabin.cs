bd9607f663e1b8989d322aaa36550a49e71d9fbf
ulsin <ulriksin@gmail.com>
2021-09-23 23:24:52 +0200

    Changed Classes to have many-to-many

--- WebappGroup9/Models/Cabin.cs
+++ WebappGroup9/Models/Cabin.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -5,17 +6,26 @@ namespace WebappGroup9.Models
 {
     public class Cabin
     {
-        /* TODO
-         * room / floor
-         * price
-         * type (string)
-         * amount of people being able to live
-         */
-
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int Id { get; set; }
 
         public string Type { get; set; }
+        // TODO Make this be like car brand and model selection
+        // Where the first dropdown causes the second one to load the option for rooms
+        public string Floor { get; set; } 
+        public string Room { get; set; }
+        public double Price { get; set; }
+        // TODO Use this to calculate what type of cabins a certain amount of customers can spend (On frontend)
+        public int Beds { get; set; }
+
+        // Setting up many to many
+        public virtual ICollection<Ticket> Tickets { get; set; }
+
+        public Cabin()
+        {
+            this.Tickets = new HashSet<Ticket>();
+        }
+        
     }
 }
