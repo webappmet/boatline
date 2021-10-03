0ac98b8f03021989d10714757cd65b0902e22adc
Erik Storås Sommer <erikssommer@gmail.com>
2021-10-03 14:40:58 +0200

    Edit error messages

--- WebappGroup9/Models/Customer.cs
+++ WebappGroup9/Models/Customer.cs
@@ -14,18 +14,17 @@ namespace WebappGroup9.Models
         [RegularExpression(@"^[a-zA-ZæøåÆØÅ. \-]{2,20}$", ErrorMessage = "Invalid Lastname")]
         public string LastName { get; set; }
 
-        [RegularExpression(@"[0-9a-zA-ZæøåÆØÅ. \-]{2,50}", ErrorMessage = "Invalid Street Adress")]
+        [RegularExpression(@"[0-9a-zA-ZæøåÆØÅ. \-]{2,50}", ErrorMessage = "Invalid Street Address")]
         public string StreetAddress { get; set; }
         
         public virtual PostalCode PostalCode { get; set; }
 
-        [RegularExpression(@"^[0-9]{8}$", ErrorMessage = "Invalid Phone")] // TODO Just to get started, needs more work
+        [RegularExpression(@"^[0-9]{8}$", ErrorMessage = "Invalid Phone number")]
         public string Phone { get; set; }
         
-        [RegularExpression(@"^[a-zA-Z0-9_\.-]+@([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,6}$", ErrorMessage = "E-mail is not valid")]
+        [RegularExpression(@"^[a-zA-Z0-9_\.-]+@([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,6}$", ErrorMessage = "Invalid Email")]
         public string Email { get; set; }
-
-        // Ha customer som hoved tabell, bestilling sterkt avhengig av customer, pizza avhengig av bestilling
+        
         public virtual List<Ticket> Tickets { get; set; }
     }
 }
