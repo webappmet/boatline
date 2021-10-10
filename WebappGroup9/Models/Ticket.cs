04be95e080c5b5dd7ca8e7dbbe12e8241ad311de
Erik Storås Sommer <erikssommer@gmail.com>
2021-10-10 23:14:16 +0200

    Improved regex

--- WebappGroup9/Models/Ticket.cs
+++ WebappGroup9/Models/Ticket.cs
@@ -1,26 +1,19 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using Microsoft.EntityFrameworkCore.Metadata.Conventions;
 
 namespace WebappGroup9.Models
 {
     public class Ticket
     {
-        // Tor uses this but i think it is for a key that doesn't autoincrement or something i dunno
-        // [Key]
-        // [System.ComponentModel.DataAnnotations.Schema.DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int Id { get; set; }
         public virtual Route Route { get; set; }
         public string Date { get; set; }
         public string StartTime { get; set; }
 
-        // Endtime will be calculated from route duration
-        // public string EndTime { get; set; }
-
         // This is how to do many to many
         public virtual ICollection<Cabin> Cabins { get; set; }
-
-
+        
         public Ticket()
         {
             this.Cabins = new HashSet<Cabin>();
