880e6ad446a57f39309515a7ba2baaf6602a55b9
Erik Storås Sommer <erikssommer@gmail.com>
2021-10-13 00:20:07 +0200

    Added plural

--- WebappGroup9/DAL/Utility.cs
+++ WebappGroup9/DAL/Utility.cs
@@ -5,20 +5,20 @@ namespace WebappGroup9.DAL
 {
     public static class Utility
     {
-        private static int _digit = 8;
+        private static int _digits = 8;
         private static readonly Random Random = new Random();
 
-        public static void UpdateDigit(int digit)
+        public static void UpdateDigits(int digits)
         {
-            _digit = digit;
+            _digits = digits;
         }
         
         public static string GetRandomHexNumber()
         {
-            byte[] buffer = new byte[_digit / 2];
+            byte[] buffer = new byte[_digits / 2];
             Random.NextBytes(buffer);
             var result = string.Concat(buffer.Select(x => x.ToString("X2")).ToArray());
-            return _digit % 2 == 0 ? result : result + Random.Next(16).ToString("X");
+            return _digits % 2 == 0 ? result : result + Random.Next(16).ToString("X");
         }
     }
 }
