43f1ba041b6654c35ae7363c4de0f19cb72581ea
Erik Storås Sommer <erikssommer@gmail.com>
2021-10-25 17:30:43 +0200

    Unit testing project

---
+++ BoatLineTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BoatLineTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public async Task Test1()
+        {
+
+        }
+    }
+}
