94fe452195c90860160003e09632ced2ad00bd69
Erik Storås Sommer <erikssommer@gmail.com>
2021-10-25 18:26:03 +0200

    Added session attributes

--- BoatLineTest/BoatLineTest.cs
+++ BoatLineTest/BoatLineTest.cs
@@ -3,6 +3,8 @@ using System.Threading.Tasks;
 using BoatLine.Controllers;
 using BoatLine.DAL.Repositories;
 using BoatLine.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 using Moq;
 using Xunit;
 
@@ -10,6 +12,15 @@ namespace BoatLineTest
 {
     public class UnitTest1
     {
+        private const string _loggedIn = "loggetInn";
+        private const string _notLoggedInn = "";
+
+        private readonly Mock<IAuthRepository> mockRep = new Mock<IAuthRepository>();
+        private readonly Mock<ILogger<AuthController>> mockLog = new Mock<ILogger<AuthController>>();
+
+        private readonly Mock<HttpContext> mockHttpContext = new Mock<HttpContext>();
+        private readonly MockHttpSession mockSession = new MockHttpSession();
+        
         [Fact]
         public async Task CreateRoute()
         {
