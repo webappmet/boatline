063f853971b6a290606f9d743f5f01961a7b7d30
Erik Storås Sommer <erikssommer@gmail.com>
2021-10-25 18:07:38 +0200

    Test method

--- BoatLineTest/UnitTest1.cs
+++ BoatLineTest/UnitTest1.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using System.Threading.Tasks;
+using BoatLine.Controllers;
+using BoatLine.DAL.Repositories;
+using BoatLine.Models;
+using Moq;
 using Xunit;
 
 namespace BoatLineTest
@@ -7,9 +11,23 @@ namespace BoatLineTest
     public class UnitTest1
     {
         [Fact]
-        public async Task Test1()
+        public async Task CreateRoute()
         {
+            var route = new Route
+            {
+                Id = 1,
+                Departure = "Oslo",
+                Destination = "Kiel",
+                DurationDays = 3,
+                DurationHours = 17
+            };
 
+            var mock = new Mock<IAuthRepository>();
+
+            mock.Setup(r => r.PostRoute(route)).ReturnsAsync(true);
+            var authController = new AuthController(mock.Object);
+            var res = await authController.PostRoute(route);
+            //Assert.True(res.ExecuteResultAsync());
         }
     }
 }
