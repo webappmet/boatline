c77e864146df4235a5127ed2b8f27a25e3d445c6
Mats Sommervold <mats.sommervold@gmail.com>
2021-11-04 04:32:11 +0100

    Refactor route selection to own component

--- BoatLine/wwwroot/src/containers/DepartureList.js
+++ BoatLine/wwwroot/src/containers/DepartureList.js
@@ -1,15 +1,12 @@
-import styled from 'styled-components';
 import { useEffect, useState } from "react";
 import Departure from "../components/content/Departure";
 import { getDeparturesByDateAndRoute } from '../api/departures';
-import { getRoutes } from '../api/routes';
 import { useHistory } from 'react-router';
 import Stack from '../components/layout/Stack';
-import Align from '../components/layout/Align';
-import Select from '../components/interface/control/Select';
 import Input from '../components/interface/control/Input';
 import List from '../components/layout/List';
 import Space from '../components/layout/Space';
+import RouteSelector from "./RouteSelector";
 
 const DepartureList = ({ items = 10, navigable = true }) => {
     const history = useHistory();
@@ -23,28 +20,9 @@ const DepartureList = ({ items = 10, navigable = true }) => {
         if (departures) setDepartures(departures);
     }
 
-    const fetchRoutes = async () => {
-        const routes = await getRoutes();
-        if (routes) {
-            routes.unshift('All');
-            setRoutes(routes);
-        }
-    }
-
-    const filterRoutes = (event) => {
-        const value = event.target.value;
-        const selectedRoute = routes.find((route) => `${route.departure} - ${route.destination}` === value)
-        if (selectedRoute) setSelectedRoute(selectedRoute);
-        else setSelectedRoute('All')
-    }
-
     const filterDate = (date) => {
         setDate(date)
     }
-
-    useEffect(() => {
-        fetchRoutes()
-    }, []);
 
     useEffect(() => {
         fetchDepartures()
@@ -54,7 +32,7 @@ const DepartureList = ({ items = 10, navigable = true }) => {
         <Stack>
             <Space>
                 <Input id="departure-date-filter" value={date} type="date" label="Filter date" validator={filterDate}/>
-                <Select changeHandler={filterRoutes} id="route-select" value={typeof selectedRoute == 'string' ? selectedRoute : `${selectedRoute.departure} - ${selectedRoute.destination}`} label="Filter route" name="Filter route" options={routes.map((route) => typeof route == 'string' ? route : `${route.departure} - ${route.destination}`)} />
+                <RouteSelector route={selectedRoute} setRoute={setSelectedRoute} id="filter-route-select" label="Filter route" all={true}/>
             </Space>
             <List count={items} navigable={navigable}>
                 {departures.map((departure, index) => {
